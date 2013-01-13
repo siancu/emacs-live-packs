9d95de505996995e7034e103baca81341a2b4962
Stelian Iancu <stelian.iancu@gmail.com>
2013-01-13 22:08:51 +0100

    Added emacs-live.el.

---
+++ emacs-live.el
@@ -0,0 +1,17 @@
+;; don't use all the official emacs-live packs
+(live-use-packs '(live/foundation-pack live/power-pack live/lang-pack live/git-pack))
+
+;; Solarized pack
+;;(live-add-packs ('~/.emacs-live-packs/solarized-pack)
+
+;; Zenburn pack
+(live-add-packs ('~/.emacs-live-packs/zenburn-pack)
+
+;; Org-mode pack
+(live-add-packs ('~/.emacs-live-packs/orgmode-pack)
+
+;; Evil-mode pack
+(live-add-packs ('~/.emacs-live-packs/evilmode-pack)
+
+;; siancu pack (has to be at the end as it contains bindings)
+(live-add-packs ('~/.emacs-live-packs/siancu-pack)
