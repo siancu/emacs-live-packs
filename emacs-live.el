33a384aa0998a6619ab0d208b43c50f3381bb630
Stelian Iancu <stelian.iancu@gmail.com>
2013-01-13 22:12:36 +0100

    Fixed some mistakes.

--- emacs-live.el
+++ emacs-live.el
@@ -2,16 +2,16 @@
 (live-use-packs '(live/foundation-pack live/power-pack live/lang-pack live/git-pack))
 
 ;; Solarized pack
-;;(live-add-packs ('~/.emacs-live-packs/solarized-pack)
+;;(live-add-packs '(~/.emacs-live-packs/solarized-pack))
 
 ;; Zenburn pack
-(live-add-packs ('~/.emacs-live-packs/zenburn-pack)
+(live-add-packs '(~/.emacs-live-packs/zenburn-pack))
 
 ;; Org-mode pack
-(live-add-packs ('~/.emacs-live-packs/orgmode-pack)
+(live-add-packs '(~/.emacs-live-packs/orgmode-pack))
 
 ;; Evil-mode pack
-(live-add-packs ('~/.emacs-live-packs/evilmode-pack)
+(live-add-packs '(~/.emacs-live-packs/evilmode-pack))
 
 ;; siancu pack (has to be at the end as it contains bindings)
-(live-add-packs ('~/.emacs-live-packs/siancu-pack)
+(live-add-packs '(~/.emacs-live-packs/siancu-pack))
